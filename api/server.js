de65f460da3646208170fcafa97da6af94a28788
Victor Braga <victorbraga.8@hotmail.com>
2024-09-25 11:18:17 -0300

    Ajuste Vercel

--- api/server.js
+++ api/server.js
@@ -1,9 +1,21 @@
+const path = require("path");
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router(path.join(__dirname, "data.geojson"));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+
+server.get("/", (req, res) => {
+  res.jsonp({ message: "Bem-vindo à API JSON-Server customizada!" });
+});
+
+server.get("/features", (req, res) => {
+  const db = router.db;
+  const features = db.get("features").value();
+  res.jsonp(features);
+});
+
 server.use(router);
 
 module.exports = server;
