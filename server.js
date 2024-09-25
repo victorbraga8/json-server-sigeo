9cf391f316a02fc027af86c8a4e6737feb21fc8a
Victor Braga <victorbraga.8@hotmail.com>
2024-09-25 11:30:59 -0300

    Ajuste Node Api

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const app = express();
+
+const geojsonPath = path.join(__dirname, "data.geojson");
+let geojsonData;
+
+const loadGeoJSON = () => {
+  try {
+    const data = fs.readFileSync(geojsonPath, "utf8");
+    geojsonData = JSON.parse(data);
+  } catch (err) {
+    geojsonData = null;
+  }
+};
+
+loadGeoJSON();
+
+app.get("/", (req, res) => {
+  res.send("API funcionando");
+});
+
+app.get("/data", (req, res) => {
+  if (geojsonData) {
+    res.json(geojsonData);
+  } else {
+    res.status(500).send("Erro ao carregar os dados.");
+  }
+});
+
+app.get("/data/features", (req, res) => {
+  if (geojsonData && geojsonData.features) {
+    res.json(geojsonData.features);
+  } else {
+    res.status(500).send("Erro ao carregar os dados.");
+  }
+});
+
+const port = process.env.PORT || 3003;
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
